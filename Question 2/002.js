518900e7cb091d23fa26c2505902da99629327cc
feeroza <160626229+feeroza@users.noreply.github.com>
2024-08-08 21:52:07 +0500

    Add files via upload

---
+++ Question 2/002.js
@@ -0,0 +1,4 @@
+//  2. Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric,
+//would you like to learn some Python today?”
+var personName = "feeroza";
+console.log("hello, ".concat(personName, " would you like some typescript today?"));
